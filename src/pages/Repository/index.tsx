28eb8fade0701a57aaaa0c738a403a7938a79f62
AntDavidLima <DavidLima@alu.ufc.br>
2020-07-05 20:11:58 -0300

    Listing issues from API

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 import { Header, RepositoryInfo, Issues } from './styles';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
@@ -10,8 +13,41 @@ interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  html_url: string;
+  title: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
+
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      setRepository(response.data);
+    });
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data);
+    });
+  }, [params.repository]);
 
   return (
     <>
@@ -22,43 +58,44 @@ const Repository: React.FC = () => {
           Voltar
         </Link>
       </Header>
-      <RepositoryInfo>
-        <header>
-          <img
-            src="https://avatars1.githubusercontent.com/u/29358300?s=460&u=8486fec35b12c3a0cb146db9d646150afcaca94e&v=4"
-            alt="David Lima"
-          />
-          <div>
-            <strong>RedaçãoUP</strong>
-            <p>
-              Minha descrição é muito loga porque eu preciso testar para saber
-              se não ocorrá nenhum erro por conta do tamanho da descrição
-            </p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>1080</strong>
-            <p>Stars</p>
-          </li>
-          <li>
-            <strong>48</strong>
-            <p>Forks</p>
-          </li>
-          <li>
-            <strong>67</strong>
-            <p>Issues abertas</p>
-          </li>
-        </ul>
-      </RepositoryInfo>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <p>Stars</p>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <p>Forks</p>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <p>Issues abertas</p>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
       <Issues>
-        <Link to="ksjhdfkshdf">
-          <div>
-            <strong>asdasdasd</strong>
-            <p>asdasdasd</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
+        {issues.map(issue => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   );
